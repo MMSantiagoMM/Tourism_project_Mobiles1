9b264e28975a3b86f90a4626375425f083a09291
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-08-08 16:54:45 -0500

    add formatter date

--- src/main/java/org/example/utilities/MyDate.java
+++ src/main/java/org/example/utilities/MyDate.java
@@ -1,7 +1,14 @@
 package org.example.utilities;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class MyDate {
 
+    public static LocalDate changeFormat(String date){
+        DateTimeFormatter newFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    
+    }
+
+
 }
